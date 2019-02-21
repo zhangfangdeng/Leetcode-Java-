09e04c14c97f1a87e7d06f4a5864cb00fedd5634
zhangfangdeng <809838715@qq.com>
2019-02-21 09:39:44 +0800

    first commit l-c-a-o-a-b-t solution

---
+++ lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,19 @@
+class Solution{
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        if(root == null)
+            return root;
+        if(root == q || root == p)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left != null && right != null)
+            return root;
+        else if(left != null)
+            return left;
+        else if(right != null)
+            return right;
+        
+        return null;
+
+    }
+}
